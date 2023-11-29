c2e01d99894cd9a78b8b83679dbace0db91bf0cc
Alexandre Yudi <alexyud@hotmail.com>
2023-11-29 07:52:35 -0300

    Preparing for deploy

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,11 +1,7 @@
 class HomeController {
 
     async index(req, res) {
-        res.send("APP EXPRESS! - Guia do programador");
-    }
-
-    async validate(req, res) {
-        res.send("Ok!");
+        res.send("Welcome");
     }
 
 }
--- routes/routes.js
+++ routes/routes.js
@@ -5,14 +5,13 @@ var UserController = require("../controllers/UserController");
 var AdminAuth = require("../middleware/AdminAuth");
 
 router.get('/', HomeController.index);
-router.post('/user', UserController.create);
-router.get('/user', AdminAuth, UserController.index);
-router.get('/user/:id', AdminAuth, UserController.findUser);
-router.put('/user', AdminAuth, UserController.edit);
-router.delete('/user/:id', AdminAuth, UserController.remove);
-router.post('/recoverpassword', UserController.recoverPassword);
-router.post('/changepassword', UserController.changePassword);
-router.post('/login', UserController.login);
-router.post('/validate', AdminAuth, HomeController.validate);
+// router.post('/user', UserController.create);
+// router.get('/user', AdminAuth, UserController.index);
+// router.get('/user/:id', AdminAuth, UserController.findUser);
+// router.put('/user', AdminAuth, UserController.edit);
+// router.delete('/user/:id', AdminAuth, UserController.remove);
+// router.post('/recoverpassword', UserController.recoverPassword);
+// router.post('/changepassword', UserController.changePassword);
+// router.post('/login', UserController.login);
 
 module.exports = router;
